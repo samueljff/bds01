c0bbb717b09668b5b8fbffc470a58a693ed99c97
Samuel Fomseca <samueljosef3@gmail.com>
2022-11-14 00:29:58 -0300

    Test Passing

---
+++ src/main/java/com/devsuperior/bds01/services/EmployeeService.java
@@ -0,0 +1,35 @@
+package com.devsuperior.bds01.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds01.dto.EmployeeDTO;
+import com.devsuperior.bds01.entities.Department;
+import com.devsuperior.bds01.entities.Employee;
+import com.devsuperior.bds01.repositories.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	private EmployeeRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<EmployeeDTO> findAllPaged(Pageable pageable){
+		Page<Employee> pages = repository.findAll(pageable);
+		return pages.map(x -> new EmployeeDTO(x));
+	}
+
+	@Transactional
+	public EmployeeDTO insert(EmployeeDTO dto) {
+		Employee employee = new Employee();
+		employee.setName(dto.getName());
+		employee.setEmail(dto.getEmail());
+		employee.setDepartment(new Department(dto.getDepartmentId(), null));
+		employee = repository.save(employee);
+		return new EmployeeDTO(employee);
+	}
+}
